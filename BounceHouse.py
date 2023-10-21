f5c9958db004e03a9e1e604ff9fe7be25af85216
DD <73483425+dandegeest@users.noreply.github.com>
2023-10-21 17:00:25 -0500

    Update BounceHouse.py
    Stub in midi function, shorten sleep, test

--- BounceHouse.py
+++ BounceHouse.py
@@ -2,14 +2,18 @@ from machine import ADC, Pin
 import utime
 
 led = Pin(25, Pin.OUT)
-
 knock = ADC(26)
-while True:
 
+def generate_midi_note(klvl):
+    #TODO: figure out how to wire up MIDI 5 pin connector and send it correct voltages   
+    print(f"D: {klvl}")
+
+while True:
     knockLvl = knock.read_u16()
     if (knockLvl > 1000):
-        print(f"D: {knockLvl}")
+        generate_midi_note(knockLvl)
         led.high()
     else:
         led.low()
-    utime.sleep_ms(10)
+    utime.sleep_ms(1)
+
