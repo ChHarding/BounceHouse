ca1c57886d2f3a7980372130b42e8bfdc745d23d
DD <73483425+dandegeest@users.noreply.github.com>
2023-10-27 18:14:54 -0500

    Update BounceHouse.py
    Add code to write a MIDI noteon/noteoff to the UART

--- BounceHouse.py
+++ BounceHouse.py
@@ -1,19 +1,31 @@
 from machine import ADC, Pin
 import utime
+import time
+import ustruct
+import random
 
 led = Pin(25, Pin.OUT)
 knock = ADC(26)
+uart = machine.UART(0,31250)
+
+#notes = [60,61,62,63,64,65,66,67,68]
+notes2 = [50,51,52,53,54,55,56,57,58]
+notes = [52,50]
 
 def generate_midi_note(klvl):
-    #TODO: figure out how to wire up MIDI 5 pin connector and send it correct voltages   
     print(f"D: {klvl}")
-
+    note = random.choice(notes)
+    led.high()
+    uart.write(ustruct.pack("bbb",0x90,note,random.randint(69,127)))
+    utime.sleep_ms(250)
+    uart.write(ustruct.pack("bbb",0x90,note,0))
+    led.low()
+        
 while True:
     knockLvl = knock.read_u16()
-    if (knockLvl > 1000):
+    if (knockLvl > 2000):
         generate_midi_note(knockLvl)
-        led.high()
     else:
         led.low()
-    utime.sleep_ms(1)
+        utime.sleep_ms(1)
 
