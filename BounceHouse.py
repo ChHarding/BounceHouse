452a9f6923bdbd1b42a5e4958fbc5cdad368079f
DD <73483425+dandegeest@users.noreply.github.com>
2023-10-28 19:08:12 -0500

    Update BounceHouse.py
    More testing and added functions for note on and off

--- BounceHouse.py
+++ BounceHouse.py
@@ -8,24 +8,34 @@ led = Pin(25, Pin.OUT)
 knock = ADC(26)
 uart = machine.UART(0,31250)
 
-#notes = [60,61,62,63,64,65,66,67,68]
-notes2 = [50,51,52,53,54,55,56,57,58]
-notes = [52,50]
+notes = [60, 61,62,63,64,65,66,67,68]
+#notes = [50,51,52,53,54,55,56,57,58]
 
+def noteOn(note, velocity):
+    if note is not None:
+        print(f"{'NOn' if velocity > 0 else 'NOff'}:{note} V:{velocity}")
+        uart.write(ustruct.pack("bbb",0x90,note,velocity))
+        return note
+    
+def noteOff(note):
+    if note is not None:
+        #uart.write(ustruct.pack("bbb",0x80,note,0))
+        noteOn(note, 0)
+    
 def generate_midi_note(klvl):
-    print(f"D: {klvl}")
-    note = random.choice(notes)
+    #print(f"D: {klvl}")      
     led.high()
-    uart.write(ustruct.pack("bbb",0x90,note,random.randint(69,127)))
-    utime.sleep_ms(250)
-    uart.write(ustruct.pack("bbb",0x90,note,0))
+    note = random.choice(notes)
+    lastNote = noteOn(note, random.randint(69,127))
+    utime.sleep_ms(200)
+    noteOff(lastNote)
     led.low()
-        
+   
 while True:
     knockLvl = knock.read_u16()
-    if (knockLvl > 2000):
+    if (knockLvl > 1500):
         generate_midi_note(knockLvl)
     else:
-        led.low()
+        led.low() 
         utime.sleep_ms(1)
 
